fdeab88d855492758e4e9fc4928eb4dfebbc4dc1
mihai-burduselu <michelcatalin@gmail.com>
2018-04-16 16:06:49 +0300

    Add login and register views

---
+++ views/login/script.js
@@ -0,0 +1,8 @@
+
+
+function login() {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    console.log(email + " " + password);
+}
---
+++ views/register/script.js
@@ -0,0 +1,11 @@
+
+
+function register() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var confirm = document.getElementById("confirm").value;
+
+    console.log(name + " " + email + " ");
+}
